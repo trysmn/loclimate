c3e31303dcdc968ea341ea3920265ac0498b9154
Trystan Newbury <trystan.newbury@thoughtworks.com>
2019-09-04 15:38:42 +0100

    backend: Convert the data in to a higher performance, readable format ready to be converted to json
    Trystan

--- backend/src/generate_data.py
+++ backend/src/generate_data.py
@@ -13,3 +13,43 @@ def convert_to_dict(csv_file_path):
         climate_array.append(month_data)
         row_index += 1
     return climate_array
+
+
+def convert_to_json(csv_file_path):
+    climate_data = pd.read_csv(csv_file_path)
+    climate_hash = {}
+    column_names = climate_data.columns[0:5]
+    row_index = 0
+    # sorted_multi_indexed_df = _get_sorted_multiindexed_df_for(climate_data)
+    while row_index < len(climate_data):
+        year = climate_data['year'][row_index]
+        year_data = {}
+        month = climate_data['month'][row_index]
+        month_data = {}
+        for metric in column_names:
+            month_data[metric] = climate_data.loc[row_index][metric]
+        year_data[month] = month_data
+        row_index += 1
+        if year in climate_hash.keys():
+            climate_hash[year][month] = month_data
+        else:
+            climate_hash[year] = year_data
+    return climate_hash
+
+
+def _get_sorted_multiindexed_df_for(climate_data: pd.DataFrame):
+    month_and_year_arrays = [list(climate_data['year']),list(climate_data['month'])]
+    tuples = list(zip(*month_and_year_arrays))
+    index = pd.MultiIndex.from_tuples(tuples, names=['year', 'month'])
+    multi_indexed_df = pd.DataFrame(
+        data=[
+            [7.7, 2.4, 5.1, 88, 43.2],
+            [6.8, 2.2, 4.5, 54.2, 36.3],
+            [7.3, 0.5, 3.9, 17.8, 39.3],
+            [9.4, 3.5, 6.5, 80.9, 44.5],
+            [7.4, 3.5, 4.5, 80.5, 43.5]
+        ],
+        columns=["max_temp", "min_temp", "mean_temp", "total_rainfall", "total_sunshine"],
+        index=index
+    )
+    return multi_indexed_df.sort_index()
--- backend/test/generators/test_generate_data.py
+++ backend/test/generators/test_generate_data.py
@@ -1,4 +1,6 @@
-from generate_data import convert_to_dict
+import pandas as pd
+
+from generate_data import convert_to_dict, convert_to_json
 
 
 def test_generate_data():
@@ -40,9 +42,72 @@ def test_generate_data():
             'total_sunshine': 44.5,
             'month': 12,
             'year': 1998
+        },
+        {
+            'max_temp': 7.4,
+            'min_temp': 3.5,
+            'mean_temp': 4.5,
+            'total_rainfall': 80.5,
+            'total_sunshine': 43.5,
+            'month': 11,
+            'year': 1998
         }
     ]
 
     result = convert_to_dict(test_data)
+
+    assert result == expected
+
+
+def test_generate_correct_json_structure():
+    test_data = "./test_csv.csv"
+
+    expected = {
+        1973: {
+            1: {
+                'max_temp': 7.7,
+                'min_temp': 2.4,
+                'mean_temp': 5.1,
+                'total_rainfall': 88.0,
+                'total_sunshine': 43.2
+            }
+        },
+        1986: {
+            5: {
+                'max_temp': 6.8,
+                'min_temp': 2.2,
+                'mean_temp': 4.5,
+                'total_rainfall': 54.2,
+                'total_sunshine': 36.3
+            }
+        },
+        2008: {
+            3: {
+                'max_temp': 7.3,
+                'min_temp': 0.5,
+                'mean_temp': 3.9,
+                'total_rainfall': 17.8,
+                'total_sunshine': 39.3
+            }
+        },
+        1998: {
+            11: {
+                'max_temp': 7.4,
+                'min_temp': 3.5,
+                'mean_temp': 4.5,
+                'total_rainfall': 80.5,
+                'total_sunshine': 43.5
+            },
+            12: {
+                'max_temp': 9.4,
+                'min_temp': 3.5,
+                'mean_temp': 6.5,
+                'total_rainfall': 80.9,
+                'total_sunshine': 44.5
+            }
+        }
+    }
+
+    result = convert_to_json(test_data)
 
     assert result == expected
