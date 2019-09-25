04bfd9b4519e5dced6241ba941d67918455d9f1b
Trystan Newbury <trystan.newbury@thoughtworks.com>
2019-09-25 14:56:53 +0100

    Frontend-hooks: Create NavBar component
    Trystan Jade

---
+++ frontend-hooks/src/NavBar/NavBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NavBar() {
+    return <h1 data-testid="app-title">loclimate</h1>
+}
+
+export default NavBar;
---
+++ frontend-hooks/src/NavBar/NavBar.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { render, getByTestId } from '@testing-library/react'
+import NavBar from "./NavBar";
+
+describe("NavBar", () => {
+    it("should have a title", () => {
+        // Given
+        const title = "loclimate";
+        const { container } = render(<NavBar />);
+        // When
+        const appTitle = getByTestId(container, "app-title")
+        // Then
+        expect(appTitle.textContent).toBe(title);
+    })
+})
