ea5ead3e08be46988b7fbd748429569f9cafa0d5
Trystan Newbury <trystan.newbury@thoughtworks.com>
2019-09-26 16:06:19 +0100

    frontend-hooks: Create select month dropdown with value of dropdown stored in state
    Jade & Trystan

--- frontend-hooks/src/ViewHeader/Dropdown/Dropdown.js
+++ frontend-hooks/src/ViewHeader/Dropdown/Dropdown.js
@@ -1,27 +1,25 @@
-import React, { useContext } from 'react'
+import React, {useContext, useState} from 'react'
 import { DropdownContext } from "../../contexts/DropdownContext";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleUp, faAngleDown } from '@fortawesome/free-solid-svg-icons'
 
 function Dropdown(props) {
+    const [value, setValue] = useState(props.title);
     const climateJson = require('../../climate_data');
     const months = Object.keys(climateJson[Object.keys(climateJson)[0]]);
-    const years = Object.keys(climateJson);
+    // const years = Object.keys(climateJson);
     const { isDropdownOpen, handleClick } = useContext(DropdownContext);
 
     return (
         <div>
-            <button onClick={handleClick}>{props.title}</button>
+            <button value={value} onClick={handleClick}>{props.title}</button>
             {isDropdownOpen
                 ? <FontAwesomeIcon icon={faAngleDown} />
                 : <FontAwesomeIcon icon={faAngleUp} />
             }
-            {isDropdownOpen && <ul>
-                {props.title === "select month" ? months.map((item) =>
-                    (<li key={item}>{item}</li> )) : years.map((item) =>
-                    (<li key={item}>{item}</li> ))
-                }
-            </ul>}
+            {isDropdownOpen && months.map((item) =>
+                (<button key={item} onClick={() => setValue(item)}>{item}</button>))
+            }
         </div>
     )
 }
